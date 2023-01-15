ef30f665707bb3282daf999e43cc67d45335bc4a
unknown <410011max@gmail.com>
2023-01-15 15:28:03 +0800

    Add Custom Model
    MLP_8x8
    MLP_16x8
    MLP_32x32
    ResNet_8x8
    
    modify some op to supported 16/8 bit

---
+++ Final_Project_Configurable_MAC/src/models/resnet/resnet.cc
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2021 The CFU-Playground Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "models/resnet/resnet.h"
+
+#include <stdio.h>
+
+#include "menu.h"
+#include "models/resnet/model_resnet.h"
+#include "models/resnet/image_data.h"
+#include "tflite.h"
+
+extern "C" {
+#include "fb_util.h"
+};
+
+// Initialize everything once
+// deallocate tensors when done
+static void resnet_init(void) {
+  tflite_load_model(model_resnet, model_resnet_len);
+}
+
+// Run classification, after input has been loaded
+static int32_t resnet_classify() {
+  printf("Running resnet\n");
+  tflite_classify();
+
+  // Process the inference results.
+  int8_t* output = tflite_get_output();
+  return output[9] - output[0];
+}
+
+static void do_classify_zeros() {
+  tflite_set_input_zeros();
+  int32_t result = resnet_classify();
+  printf("  result is %ld\n", result);
+}
+
+static void do_classify() {
+  tflite_set_input(resnet_data);
+  int32_t result = resnet_classify();
+  printf("  result is %ld\n", result);
+}
+
+#define NUM_GOLDEN 2
+static int32_t golden_results[NUM_GOLDEN] = {-59, 65};
+
+static void do_golden_tests() {
+  int32_t actual[NUM_GOLDEN];
+
+  tflite_set_input_zeros();
+  actual[0] = resnet_classify();
+  
+  tflite_set_input(resnet_data);
+  actual[1] = resnet_classify();
+
+  bool failed = false;
+  for (size_t i = 0; i < NUM_GOLDEN; i++) {
+    if (actual[i] != golden_results[i]) {
+      failed = true;
+      printf("*** Golden test %d failed: %ld (actual) != %ld (expected))\n", i,
+             actual[i], golden_results[i]);
+    }
+  }
+
+  if (failed) {
+    puts("FAIL Golden tests failed");
+  } else {
+    puts("OK   Golden tests passed");
+  }
+}
+
+static struct Menu MENU = {
+    "Tests for Resnet model",
+    "test",
+    {
+        MENU_ITEM('1', "Run with zeros input", do_classify_zeros),
+        MENU_ITEM('2', "Run with random input", do_classify),
+        MENU_ITEM('g', "Run golden tests (check for expected outputs)",
+                  do_golden_tests),
+        MENU_END,
+    },
+};
+
+// For integration into menu system
+void resnet_menu() {
+  resnet_init();
+  
+  menu_run(&MENU);
+}
